a1c06a7cbf470f6328600cae98bfa85e362c8a4a
user <user@Natalie-PC.ghb.hs-furtwangen.de>
2017-10-11 21:05:06 +0200

    Dateien erstellt

---
+++ a1/a1.js
@@ -0,0 +1,8 @@
+/* Aufgabe: 0
+    Name: Natalie D�ll
+    Matrikel: 256026
+    Datum: 11.10.2017
+    Hiermit versichere ich, dass ich diesen
+    Code selbst geschrieben habe. Er wurde
+    nicht kopiert und auch nicht diktiert. */
+//# sourceMappingURL=a1.js.map
