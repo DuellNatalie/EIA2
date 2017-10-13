1485f27d16915c6317c8e3eaa6c0a2078405c4c3
duellnat <duellnat@141.28.124.33>
2017-10-13 10:27:55 +0200

    Canvas

--- a1/a1.js
+++ a1/a1.js
@@ -15,6 +15,7 @@ function init() {
     crc2.beginPath();
     crc2.moveTo(0, 50);
     crc2.lineTo(0, 600);
+    crc2.stroke();
     crc2.lineTo(800, 600);
     crc2.closePath();
     crc2.fillStyle = "#ffffff";
--- a1/a1.ts
+++ a1/a1.ts
@@ -19,6 +19,7 @@ function init (): void {
     crc2.beginPath();
     crc2.moveTo(0,50);
     crc2.lineTo(0,600);
+    crc2.stroke();
     crc2.lineTo(800,600);
     crc2.closePath();
     crc2.fillStyle="#ffffff";
