af5e0d5300e35de857cd536a34994f3112324169
duellnat <duellnat@141.28.124.33>
2017-10-13 10:18:37 +0200

    Canvas

--- a1/a1.ts
+++ a1/a1.ts
@@ -28,4 +28,15 @@ function init (): void {
     crc2.stroke();
     crc2.fillStyle="#00ff00";
     crc2.fill();
+    
+    crc2.beginPath();
+    crc2.moveTo(0,50);
+    crc2.lineTo(0,0);
+    crc2.lineTo(800,0);
+    crc2.lineTo(800,600);
+    crc2.closePath();
+    crc2.stroke();
+    crc2.fillStyle="#0000ff";
+    crc2.fill();
+    
     }
