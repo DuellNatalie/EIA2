99b9e881089e077b775388dfd25deb208dc58b7e
duellnat <duellnat@141.28.124.33>
2017-10-13 10:20:01 +0200

    Canvas

--- a1/a1.js
+++ a1/a1.js
@@ -29,8 +29,8 @@ function init() {
     crc2.stroke();
     crc2.fillStyle = "#0000ff";
     crc2.fill();
-    crc2.moveTo(0, 0);
-    crc2.lineTo(800, 550);
+    crc2.moveTo(0, 30);
+    crc2.lineTo(800, 570);
     crc2.stroke();
 }
 //# sourceMappingURL=a1.js.map
--- a1/a1.ts
+++ a1/a1.ts
@@ -35,7 +35,7 @@ function init (): void {
     crc2.fillStyle="#0000ff";
     crc2.fill();
     
-     crc2.moveTo(0,0)
-    crc2.lineTo(800,550);
+     crc2.moveTo(0,30)
+    crc2.lineTo(800,570);
     crc2.stroke();
     }
