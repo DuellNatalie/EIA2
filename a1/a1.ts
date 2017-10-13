a9a702c3fdf77a4745a1b091fd1a996456ea2048
duellnat <duellnat@141.28.124.33>
2017-10-13 10:27:23 +0200

    Canvas

--- a1/a1.ts
+++ a1/a1.ts
@@ -19,9 +19,8 @@ function init (): void {
     crc2.beginPath();
     crc2.moveTo(0,50);
     crc2.lineTo(0,600);
-    crc2.lineTo(600,600);
+    crc2.lineTo(800,600);
     crc2.closePath();
-    crc2.stroke();
     crc2.fillStyle="#ffffff";
     crc2.fill();
     
