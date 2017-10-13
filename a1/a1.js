aa1a0900e4ededb66e60fee085330c5df225849a
duellnat <duellnat@141.28.124.33>
2017-10-13 10:50:55 +0200

    Canvas

--- a1/a1.js
+++ a1/a1.js
@@ -20,6 +20,7 @@ var a1;
         crc2.lineTo(800, 600);
         crc2.closePath();
         crc2.fillStyle = "#ffffff";
+        crc2.strokeStyle = "#ff0000";
         crc2.stroke();
         crc2.fill();
         /*Himmel*/
@@ -28,9 +29,9 @@ var a1;
         crc2.lineTo(0, 0);
         crc2.lineTo(800, 0);
         crc2.lineTo(800, 600);
+        crc2.closePath();
         crc2.strokeStyle = "#ff0000";
         crc2.stroke();
-        crc2.closePath();
         crc2.fillStyle = "#CEECF5";
         crc2.fill();
         /*Lift-Linie*/
--- a1/a1.ts
+++ a1/a1.ts
@@ -10,8 +10,9 @@
 
 namespace a1 {
    
-    window.addEventListener("load", init);
-function init (): void {
+window.addEventListener("load", init);
+ 
+    function init (): void {
     let canvas: HTMLCanvasElement = document.getElementsByTagName("canvas")[0];
     console.log(canvas);
     
@@ -25,6 +26,7 @@ function init (): void {
     crc2.lineTo(800,600);
     crc2.closePath();
     crc2.fillStyle="#ffffff";
+    crc2.strokeStyle="#ff0000";
     crc2.stroke();
     crc2.fill();
     
@@ -34,9 +36,9 @@ function init (): void {
     crc2.lineTo(0,0);
     crc2.lineTo(800,0);
     crc2.lineTo(800,600);
-    crc2.strokeStyle="#ff0000";
-    crc2.stroke();
     crc2.closePath();
+    crc2.strokeStyle="#ff0000";
+    crc2.stroke(); 
     crc2.fillStyle="#CEECF5";
     crc2.fill();
     
