bdce542d5fafa51b1db40f90a6767b989abebe89
duellnat <duellnat@141.28.124.33>
2017-10-13 10:16:54 +0200

    Canvas

--- a1/a1.js
+++ a1/a1.js
@@ -17,8 +17,8 @@ function init() {
     crc2.stroke();
     crc2.beginPath();
     crc2.moveTo(0, 50);
-    crc2.lineTo(0, 800);
-    crc2.lineTo(600, 800);
+    crc2.lineTo(0, 600);
+    crc2.lineTo(800, 600);
     crc2.closePath();
     crc2.stroke();
     crc2.fillStyle = "#00ff00";
