ed19600dc55648676f7cae92efe8c8720a3c61a1
duellnat <duellnat@141.28.124.33>
2017-10-13 10:28:37 +0200

    Canvas

--- a1/a1.js
+++ a1/a1.js
@@ -15,10 +15,10 @@ function init() {
     crc2.beginPath();
     crc2.moveTo(0, 50);
     crc2.lineTo(0, 600);
-    crc2.stroke();
     crc2.lineTo(800, 600);
     crc2.closePath();
-    crc2.fillStyle = "#ffffff";
+    crc2.stroke();
+    crc2.fillStyle = "#ffff00";
     crc2.fill();
     /*Himmel*/
     crc2.beginPath();
