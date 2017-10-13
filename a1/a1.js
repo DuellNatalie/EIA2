c6ab11f18d3afc74ca073b9c10c5749a5c96dc6b
user <user@Natalie-PC.ghb.hs-furtwangen.de>
2017-10-13 15:14:45 +0200

    Piste und Berge

--- a1/a1.js
+++ a1/a1.js
@@ -14,17 +14,27 @@ var a1;
         var crc2 = canvas.getContext("2d");
         console.log(crc2);
         //Himmel
-        crc2.fillStyle = "#CEECF5";
+        crc2.fillStyle = "#ceecf5";
         crc2.fillRect(0, 0, 800, 600);
+        //Ganz hinterer Berg  
+        crc2.beginPath();
+        crc2.strokeStyle = "#afbecf";
+        crc2.moveTo(590, 350);
+        crc2.lineTo(300, 600);
+        crc2.lineTo(700, 600);
+        crc2.closePath();
+        crc2.stroke();
+        crc2.fillStyle = "#afbecf";
+        crc2.fill();
         //Hinterer Berg  
         crc2.beginPath();
-        crc2.moveTo(150, 112);
-        crc2.lineTo(200, 50);
-        crc2.lineTo(750, 562);
+        crc2.strokeStyle = "#8a98a8";
+        crc2.moveTo(700, 300);
+        crc2.lineTo(400, 600);
+        crc2.lineTo(900, 600);
         crc2.closePath();
-        crc2.strokeStyle = "##3d668f";
         crc2.stroke();
-        crc2.fillStyle = "#3d668f";
+        crc2.fillStyle = "#8a98a8";
         crc2.fill();
         //Berg
         crc2.beginPath();
@@ -32,9 +42,9 @@ var a1;
         crc2.lineTo(800, 600);
         crc2.lineTo(0, 600);
         crc2.closePath();
-        crc2.strokeStyle = "#FFFFFF";
+        crc2.strokeStyle = "#ffffff";
         crc2.stroke();
-        crc2.fillStyle = "#FFFFFF";
+        crc2.fillStyle = "#ffffff";
         crc2.fill();
     }
 })(a1 || (a1 = {}));
--- a1/a1.ts
+++ a1/a1.ts
@@ -21,20 +21,31 @@ window.addEventListener("load", init);
     
 
 //Himmel
-    crc2.fillStyle = "#CEECF5";
+    crc2.fillStyle = "#ceecf5";
     crc2.fillRect (0, 0, 800, 600);
     
-        
+      //Ganz hinterer Berg  
+    crc2.beginPath();
+    crc2.strokeStyle = "#afbecf";
+    crc2.moveTo(590, 350); 
+    crc2.lineTo(300, 600); 
+    crc2.lineTo(700, 600); 
+    crc2.closePath(); 
+    crc2.stroke(); 
+    crc2.fillStyle = "#afbecf";
+    crc2.fill()
+           
 //Hinterer Berg  
     crc2.beginPath();
-    crc2.moveTo(150, 112);
-    crc2.lineTo(200, 50);
-    crc2.lineTo(750, 562);
-    crc2.closePath();
-    crc2.strokeStyle = "##3d668f";
-    crc2.stroke();
-    crc2.fillStyle = "#3d668f";
+    crc2.strokeStyle = "#8a98a8";
+    crc2.moveTo(700, 300); 
+    crc2.lineTo(400, 600); 
+    crc2.lineTo(900, 600); 
+    crc2.closePath(); 
+    crc2.stroke(); 
+    crc2.fillStyle = "#8a98a8";
     crc2.fill();
+
         
 //Berg
     crc2.beginPath();
@@ -42,9 +53,9 @@ window.addEventListener("load", init);
     crc2.lineTo(800, 600);
     crc2.lineTo(0,600);
     crc2.closePath();
-    crc2.strokeStyle="#FFFFFF";
+    crc2.strokeStyle="#ffffff";
     crc2.stroke();
-    crc2.fillStyle = "#FFFFFF";
+    crc2.fillStyle = "#ffffff";
     crc2.fill();
         
 
