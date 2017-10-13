dcb15218e2b2991c3a4d4273c2d138e43a491163
user <user@Natalie-PC.ghb.hs-furtwangen.de>
2017-10-13 14:29:22 +0200

    Canvas

--- a1/a1.ts
+++ a1/a1.ts
@@ -19,34 +19,32 @@ window.addEventListener("load", init);
     let crc2:CanvasRenderingContext2D = canvas.getContext("2d");
     console.log(crc2)
     
-        /*Himmel*/
-    crc2.strokeStyle="#ff0000";
-    crc2.fillStyle="#CEECF5";
-    crc2.rect(0,0,800,600)
-    crc2.fill();
-    crc2.stroke(); 
+
+//Himmel
+    crc2.fillStyle = "#CEECF5";
+    crc2.fillRect (0, 0, 800, 600);
     
-   /*-----------------------------*/ 
-        
-   
-//    crc2.strokeStyle="#ff0000";
-//    crc2.stroke();
+//Berg
     crc2.beginPath();
-    crc2.moveTo(0,50);
-    crc2.lineTo(0,600);
-    crc2.lineTo(800,600);
-    
+    crc2.moveTo(150, 112);
+    crc2.lineTo(200, 50);
+    crc2.lineTo(750, 562);
     crc2.closePath();
+    crc2.strokeStyle = "#c2c2c2";
+    crc2.stroke();
+    crc2.fillStyle = "#c2c2c2";
+    crc2.fill();
     
-//    crc2.fillStyle="#ffffff"; 
-//    crc2.fill();
-//    
-//    
-/*Lift-Linie*/
-crc2.moveTo(0,20)
-crc2.lineTo(800,570);
-crc2.strokeStyle="#000000";
-crc2.stroke();
+    //Piste
+    crc2.beginPath();
+    crc2.moveTo(0, 0);
+    crc2.lineTo(800, 600);
+    crc2.lineTo(0,600);
+    crc2.closePath();
+    crc2.strokeStyle="#FFFFFF";
+    crc2.stroke();
+    crc2.fillStyle = "#FFFFFF";
+    crc2.fill();
   
     }
     
