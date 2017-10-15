129d391fe285ee3eb1bb016ea26c01168d3be354
user <user@Natalie-PC.ghb.hs-furtwangen.de>
2017-10-15 13:25:20 +0200

    Skipiste fertiggestellt

--- a1/a1.js
+++ a1/a1.js
@@ -1,7 +1,7 @@
-/* Aufgabe: 0
+/* Aufgabe: 1
     Name: Natalie D�ll
     Matrikel: 256026
-    Datum: 11.10.2017
+    Datum: 14.10.2017
     Hiermit versichere ich, dass ich diesen
     Code selbst geschrieben habe. Er wurde
     nicht kopiert und auch nicht diktiert. */
@@ -45,6 +45,55 @@ var a1;
         crc2.strokeStyle = "#ffffff";
         crc2.stroke();
         crc2.fillStyle = "#ffffff";
+        crc2.fill();
+        //Lift-Linie
+        crc2.beginPath();
+        crc2.strokeStyle = "#000000";
+        crc2.moveTo(50, 0);
+        crc2.lineTo(800, 550);
+        crc2.stroke();
+        //Baum 3
+        crc2.beginPath();
+        crc2.strokeStyle = "#265941";
+        crc2.moveTo(70, 500);
+        crc2.lineTo(190, 500);
+        crc2.lineTo(140, 350);
+        crc2.closePath();
+        crc2.stroke();
+        crc2.fillStyle = "#265941";
+        crc2.fill();
+        //Baum 2
+        crc2.beginPath();
+        crc2.strokeStyle = "#004d26";
+        crc2.moveTo(130, 570);
+        crc2.lineTo(250, 570);
+        crc2.lineTo(200, 420);
+        crc2.closePath();
+        crc2.stroke();
+        crc2.fillStyle = "#004d26";
+        crc2.fill();
+        //Baum1
+        crc2.beginPath();
+        crc2.strokeStyle = "#113b27";
+        crc2.moveTo(50, 600);
+        crc2.lineTo(170, 600);
+        crc2.lineTo(110, 450);
+        crc2.closePath();
+        crc2.stroke();
+        crc2.fillStyle = "#113b27";
+        crc2.fill();
+        //Wolke
+        crc2.beginPath();
+        crc2.strokeStyle = "#ffffff";
+        crc2.arc(500, 125, 23, 0, 2 * Math.PI);
+        crc2.fillStyle = "#ffffff";
+        crc2.fill();
+        crc2.beginPath();
+        crc2.arc(523, 108, 25, 0, 2 * Math.PI);
+        crc2.fill();
+        crc2.beginPath();
+        crc2.arc(535, 129, 22, 0, 2 * Math.PI);
+        ;
         crc2.fill();
     }
 })(a1 || (a1 = {}));
