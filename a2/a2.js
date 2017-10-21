251156e07ad14e0d942124a10af4fccc994fd886
user <user@Natalie-PC.ghb.hs-furtwangen.de>
2017-10-21 17:46:58 +0200

    Fertiggestellt

--- a2/a2.js
+++ a2/a2.js
@@ -1,7 +1,7 @@
 /* Aufgabe: 2
     Name: Natalie D�ll
     Matrikel: 256026
-    Datum: 20.10.2017
+    Datum: 21.10.2017
     Hiermit versichere ich, dass ich diesen
     Code selbst geschrieben habe. Er wurde
     nicht kopiert und auch nicht diktiert. */
@@ -53,65 +53,52 @@ var a2;
         crc2.moveTo(50, 0);
         crc2.lineTo(800, 555);
         crc2.stroke();
-        //Baum 3
-        crc2.beginPath();
-        crc2.strokeStyle = "#265941";
-        crc2.moveTo(70, 500);
-        crc2.lineTo(190, 500);
-        crc2.lineTo(140, 350);
-        crc2.closePath();
-        crc2.stroke();
-        crc2.fillStyle = "#265941";
-        crc2.fill();
-        //Baum 2
-        crc2.beginPath();
-        crc2.strokeStyle = "#004d26";
-        crc2.moveTo(130, 570);
-        crc2.lineTo(250, 570);
-        crc2.lineTo(200, 420);
-        crc2.closePath();
-        crc2.stroke();
-        crc2.fillStyle = "#004d26";
-        crc2.fill();
-        //Baum1
-        crc2.beginPath();
-        crc2.strokeStyle = "#113b27";
-        crc2.moveTo(50, 600);
-        crc2.lineTo(170, 600);
-        crc2.lineTo(110, 450);
-        crc2.closePath();
-        crc2.stroke();
-        crc2.fillStyle = "#113b27";
-        crc2.fill();
-        //Wolke
-        crc2.beginPath();
-        crc2.strokeStyle = "#ffffff";
-        crc2.arc(500, 125, 23, 0, 2 * Math.PI);
-        crc2.fillStyle = "#ffffff";
-        crc2.fill();
-        crc2.beginPath();
-        crc2.arc(523, 108, 25, 0, 2 * Math.PI);
-        crc2.fill();
-        crc2.beginPath();
-        crc2.arc(535, 129, 22, 0, 2 * Math.PI);
-        ;
-        crc2.fill();
         //B�ume
-        for (let i = 0; i < 7; i++) {
-            let x = 20 + Math.random() * 380;
-            let y = 280 + Math.random() * 300;
-            drawTree(x, y, "#113b27");
+        for (let i = 0; i < 6; i++) {
+            let x = 60 + Math.random() * 380;
+            let y = 200 + Math.random() * 230;
+            drawTree(x, y);
+        }
+        //Schnee
+        for (let i = 0; i < 300; i++) {
+            let x = 10 + Math.random() * 780;
+            let y = 10 + Math.random() * 580;
+            drawSnow(x, y);
+        }
+        //Wolken
+        for (let i = 0; i < 3; i++) {
+            let x = 300 + Math.random() * 420;
+            let y = 60 + Math.random() * 90;
+            drawCloud(x, y);
         }
     }
-    function drawTree(_x, _y, _color) {
+    function drawTree(_x, _y) {
         crc2.beginPath();
         crc2.moveTo(_x, _y);
         crc2.lineTo(_x + 50, _y + 150);
         crc2.lineTo(_x - 50, _y + 150);
         crc2.closePath();
-        crc2.strokeStyle = _color;
+        crc2.strokeStyle = "#004d26";
         crc2.stroke();
-        crc2.fillStyle = _color;
+        crc2.fillStyle = "#113b27";
+        crc2.fill();
+    }
+    function drawSnow(x, y) {
+        crc2.beginPath();
+        crc2.arc(x, y, 4, 0, 2 * Math.PI);
+        crc2.fillStyle = "#ffffff";
+        crc2.fill();
+    }
+    function drawCloud(x, y) {
+        crc2.beginPath();
+        crc2.arc(x, y, 23, 0, 2 * Math.PI);
+        crc2.fillStyle = "#ffffff";
+        crc2.fill();
+        crc2.beginPath();
+        crc2.arc(x + 23, y - 13, 25, 0, 2 * Math.PI);
+        crc2.fill();
+        crc2.beginPath();
+        crc2.arc(x + 36, y + 4, 22, 0, 2 * Math.PI);
         crc2.fill();
     }
 })(a2 || (a2 = {}));
--- a2/a2.ts
+++ a2/a2.ts
@@ -1,7 +1,7 @@
 /* Aufgabe: 2
     Name: Natalie Düll
     Matrikel: 256026
-    Datum: 20.10.2017
+    Datum: 21.10.2017
     Hiermit versichere ich, dass ich diesen
     Code selbst geschrieben habe. Er wurde
     nicht kopiert und auch nicht diktiert. */
@@ -15,16 +15,16 @@ namespace a2 {
     let crc2: CanvasRenderingContext2D
 
     function init(): void {
-        
+
         let canvas: HTMLCanvasElement = document.getElementsByTagName("canvas")[0];
         console.log(canvas);
-
         crc2 = canvas.getContext("2d");
         console.log(crc2);
 
         //Himmel
         crc2.fillStyle = "#ceecf5";
         crc2.fillRect(0, 0, 800, 600);
+        
         //Ganz hinterer Berg  
         crc2.beginPath();
         crc2.strokeStyle = "#afbecf";
@@ -35,6 +35,7 @@ namespace a2 {
         crc2.stroke();
         crc2.fillStyle = "#afbecf";
         crc2.fill()
+        
         //Hinterer Berg  
         crc2.beginPath();
         crc2.strokeStyle = "#8a98a8";
@@ -45,6 +46,7 @@ namespace a2 {
         crc2.stroke();
         crc2.fillStyle = "#8a98a8";
         crc2.fill();
+        
         //Berg
         crc2.beginPath();
         crc2.moveTo(0, 0);
@@ -55,78 +57,67 @@ namespace a2 {
         crc2.stroke();
         crc2.fillStyle = "#ffffff";
         crc2.fill();
+        
         //Lift-Linie
         crc2.beginPath();
         crc2.strokeStyle = "#000000";
         crc2.moveTo(50, 0);
         crc2.lineTo(800, 555);
         crc2.stroke();
-        //Baum 3
-        crc2.beginPath();
-        crc2.strokeStyle = "#265941";
-        crc2.moveTo(70, 500);
-        crc2.lineTo(190, 500);
-        crc2.lineTo(140, 350);
-        crc2.closePath();
-        crc2.stroke();
-        crc2.fillStyle = "#265941";
-        crc2.fill();
-        //Baum 2
-        crc2.beginPath();
-        crc2.strokeStyle = "#004d26";
-        crc2.moveTo(130, 570);
-        crc2.lineTo(250, 570);
-        crc2.lineTo(200, 420);
-        crc2.closePath();
-        crc2.stroke();
-        crc2.fillStyle = "#004d26";
-        crc2.fill();
-        //Baum1
-        crc2.beginPath();
-        crc2.strokeStyle = "#113b27";
-        crc2.moveTo(50, 600);
-        crc2.lineTo(170, 600);
-        crc2.lineTo(110, 450);
-        crc2.closePath();
-        crc2.stroke();
-        crc2.fillStyle = "#113b27";
-        crc2.fill();
-        //Wolke
-        crc2.beginPath();
-        crc2.strokeStyle = "#ffffff";
-        crc2.arc(500, 125, 23, 0, 2 * Math.PI);
-        crc2.fillStyle = "#ffffff";
-        crc2.fill();
-        crc2.beginPath();
-        crc2.arc(523, 108, 25, 0, 2 * Math.PI);
-        crc2.fill();
-        crc2.beginPath();
-        crc2.arc(535, 129, 22, 0, 2 * Math.PI);;
-        crc2.fill();
+
 
         //Bäume
-        for (let i:number = 0; i < 7; i++) {
-            let x: number = 20 + Math.random() * 380;
-            let y: number = 280 + Math.random() * 300;
-            drawTree(x, y, "#113b27");
+        for (let i: number = 0; i < 6; i++) {
+            let x: number = 60 + Math.random() * 380;
+            let y: number = 200 + Math.random() * 230;
+            drawTree(x, y);
+        }
+        
+        //Schnee
+        for (let i: number = 0; i < 300; i++) {
+            let x: number = 10 + Math.random() * 780;
+            let y: number = 10 + Math.random() * 580;
+            drawSnow(x, y);
         }
 
-
+        //Wolken
+        for (let i: number = 0; i < 3; i++) {
+            let x: number = 300 + Math.random() * 420;
+            let y: number = 60 + Math.random() * 90;
+            drawCloud(x, y);
+        }
     }
 
-    function drawTree(_x:number, _y:number, _color:string): void {
-
+    function drawTree(_x: number, _y: number): void {
         crc2.beginPath();
         crc2.moveTo(_x, _y);
         crc2.lineTo(_x + 50, _y + 150);
         crc2.lineTo(_x - 50, _y + 150);
         crc2.closePath();
-        crc2.strokeStyle = _color;
+        crc2.strokeStyle = "#004d26";
         crc2.stroke();
-        crc2.fillStyle = _color;
+        crc2.fillStyle = "#113b27";
         crc2.fill();
     }
 
+    function drawSnow(x: number, y: number): void {
+        crc2.beginPath();
+        crc2.arc(x, y, 4, 0, 2 * Math.PI);
+        crc2.fillStyle = "#ffffff";
+        crc2.fill();
+    }
 
+    function drawCloud(x: number, y: number): void {
+        crc2.beginPath();
+        crc2.arc(x, y, 23, 0, 2 * Math.PI);
+        crc2.fillStyle = "#ffffff";
+        crc2.fill();
+        crc2.beginPath();
+        crc2.arc(x + 23, y - 13, 25, 0, 2 * Math.PI);
+        crc2.fill();
+        crc2.beginPath();
+        crc2.arc(x + 36, y + 4, 22, 0, 2 * Math.PI);
+        crc2.fill();
 
+    }
 }
