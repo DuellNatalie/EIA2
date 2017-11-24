67bc753c71f51e678094627589f36fddbddb6500
duellnat <duellnat@141.28.124.33>
2017-11-24 09:56:57 +0100

    x

---
+++ a6/move.js
@@ -0,0 +1,14 @@
+/* Aufgabe: 6
+    Name: Natalie D�ll
+    Matrikel: 256026
+    Datum: 24.11.2017
+    Hiermit versichere ich, dass ich diesen
+    Code selbst geschrieben habe. Er wurde
+    nicht kopiert und auch nicht diktiert. */
+var a6;
+(function (a6) {
+    class move {
+    }
+    a6.move = move;
+})(a6 || (a6 = {}));
+//# sourceMappingURL=move.js.map
---
+++ a6/move.ts
@@ -0,0 +1,41 @@
+/* Aufgabe: 6
+    Name: Natalie Düll
+    Matrikel: 256026
+    Datum: 24.11.2017
+    Hiermit versichere ich, dass ich diesen
+    Code selbst geschrieben habe. Er wurde
+    nicht kopiert und auch nicht diktiert. */
+
+namespace a6 {
+    
+   export class move { 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+                 }  
+        }
