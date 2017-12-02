8761bb17d78b9d291bb5d52dda7fbba955844539
user <user@Natalie-PC.ghb.hs-furtwangen.de>
2017-12-02 22:32:18 +0100

    queryData erstellt

--- a7/a7.js
+++ a7/a7.js
@@ -14,7 +14,7 @@ var StudiVZ;
         switch (action) {
             case "n":
             case "N":
-                var input = prompt("Eingabe (jeweils mit Komma getrennt) von\nMatrikelnummer, Name, Vorname, Alter, Geschlecht (m oder w) und Kommentar");
+                var input = prompt("Eingabe (jeweils mit Komma getrennt) von\nMatrikelnummer, Name, Vorname, Alter, Geschlecht (0 (m) oder 1 (w)) und Kommentar");
                 alert(saveData(input));
                 break;
             case "a":
@@ -28,16 +28,31 @@ var StudiVZ;
         }
     }
     function saveData(_input) {
-        var splitted = _input.split(',');
-        for (var matrikel in splitted) {
+        let splitted = _input.split(',');
+        let studentDat = {
+            matrikel: parseInt(splitted[0]),
+            lastname: splitted[1],
+            firstname: splitted[2],
+            age: parseInt(splitted[3]),
+            gender: parseInt(splitted[4]) == 0,
+            com: splitted[5],
+        };
+        if (Number.isNaN(studentDat.matrikel)) {
+            return "Matrikelnummer falsch";
         }
-        return "Hier fehlt noch der richtige Code...";
+        students.push(studentDat);
+        return "Deine Daten: " + "\nMatrikelnummer: " + studentDat.matrikel + "\nName: " + studentDat.lastname + "\nVorname: " + studentDat.firstname + "\nAlter: " + studentDat.age + "\nGeschlecht: " + studentDat.gender + "\nKommentar: " + studentDat.com;
     }
-    function queryData(_matrikel) {
-        return "Hier fehlt noch der richtige Code...";
-        //         if( ){ //Daten gespeichert -> zur�ckgeben
-        //            }        
-        //       else {return "Diese Daten sind uns nicht bekannt!"}
+    function queryData(_matr) {
+        for (let i = 0; i < students.length; i++) {
+            if (students[i].matrikel == _matr) {
+                return "Deine Daten: " + "\nMatrikelnummer: " + students[i].matrikel + "\nName: " + students[i].lastname + "\nVorname: " + students[i].firstname + "\nAlter: " + students[i].age + "\nGeschlecht: " + students[i].gender + "\nKommentar: " + students[i].com;
+            }
+            else {
+                continue;
+            }
+        }
+        return "Matrikelnummer nicht gespeichert";
     }
 })(StudiVZ || (StudiVZ = {}));
 //# sourceMappingURL=a7.js.map
