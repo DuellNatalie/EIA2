b24a263a8e5a1432ae353ac6ebf714c542f52826
user <user@Natalie-PC.ghb.hs-furtwangen.de>
2017-12-06 14:49:24 +0100

    Semesteraufgabe EIA 1

---
+++ Semesteraufgabe/Semesteraufgabe.js
@@ -0,0 +1,16 @@
+/* Aufgabe: Semesteraufgabe - Portfolio
+    Name: Natalie D�ll
+    Matrikel: 256026
+    Datum: 19.06.2017
+    Hiermit versichere ich, dass ich diesen
+    Code selbst geschrieben habe. Er wurde
+    nicht kopiert und auch nicht diktiert. */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+function topFunction() {
+    document.body.scrollTop = 100;
+    document.documentElement.scrollTop = 100;
+}
+/*Schleife -> Siehe JS*/ 
+//# sourceMappingURL=Semesteraufgabe.js.map
