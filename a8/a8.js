ea0259844daa48e2907b458dbc05a1484096e7c2
duellnat <duellnat@141.28.124.33>
2017-12-08 09:21:27 +0100

    a8 function init fertig

--- a8/a8.js
+++ a8/a8.js
@@ -9,6 +9,25 @@ var a8;
 (function (a8) {
     window.addEventListener("load", input);
     function input() {
+        let n = prompt("Bitte gib eine Zahl zwischen 10 und 100 ein!");
+        if (Number.isNaN(parseInt(n)) || parseInt(n) < 10 || parseInt(n) > 100) {
+            alert("Versuche es noch einmal!");
+            input();
+        }
+        else {
+            for (let i = 0; i < parseInt(n); i++) {
+                init(Math.random() * window.innerWidth - 130, Math.random() * window.innerHeight - 130, Math.random() * 360);
+            }
+        }
+    }
+    function init(x, y, color) {
+        let div = document.createElement("div");
+        div.style.width = "40px";
+        div.style.height = "40px";
+        div.style.backgroundColor = "hsl(" + color + ", 100%, 35%)";
+        div.style.left = x + "px";
+        div.style.top = y + "px";
+        document.body.appendChild(div);
     }
 })(a8 || (a8 = {}));
 //# sourceMappingURL=a8.js.map
--- a8/a8.ts
+++ a8/a8.ts
@@ -11,9 +11,37 @@ namespace a8
 
     window.addEventListener("load", input);
     
-    function input () : void    { 
-    
+    function input () : void    {
         
+        let n = prompt("Bitte gib eine Zahl zwischen 10 und 100 ein!");
+        
+        if (Number.isNaN(parseInt(n)) || parseInt(n) < 10 || parseInt(n) > 100) {
+            alert("Versuche es noch einmal!");
+            
+            input();
         }
+        
+        else {
+            for (let i = 0; i < parseInt(n); i++) {
+                init(Math.random() * window.innerWidth - 130, Math.random() * window.innerHeight - 130, Math.random() * 360);
+            }
+        }
+        
+    }
     
- }
+    function init (x : number, y : number, color : number) : void {
+        
+        let div = document.createElement("div");
+        
+        div.style.width = "40px";
+        div.style.height = "40px";
+        
+        div.style.backgroundColor = "hsl(" + color + ", 100%, 35%)";
+        
+        div.style.left = x + "px";
+        div.style.top = y + "px";
+        
+        document.body.appendChild(div);
+    }
+    
+}
