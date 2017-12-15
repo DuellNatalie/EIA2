0520c7cef83fbdba0485e7c133ca57f72eb7ec24
Natalie2 <Natalie2@natalieLaptop.eduroam.hs-furtwangen.de>
2017-12-15 10:10:31 +0100

    Dateien angelegt

---
+++ a9/a9.js
@@ -0,0 +1,8 @@
+/*  Aufgabe 9
+    Name: Natalie D�ll
+    Matrikel: 256026
+    Datum: 15.12.2017
+    Hiermit versichere ich, dass ich diesen
+    Code selbst geschrieben habe. Er wurde
+    nicht kopiert und auch nicht diktiert.*/
+//# sourceMappingURL=a9.js.map
