47d7ff5a0df6e2ba2a88cdbd79a56b1e9ec8bad3
user <user@Natalie-PC.ghb.hs-furtwangen.de>
2018-01-11 15:08:06 +0100

    function init: letters erstellt & gestylt

--- a9/a9.js
+++ a9/a9.js
@@ -9,16 +9,33 @@ var a9;
 (function (a9) {
     window.addEventListener("load", init);
     let Letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-    let div = document.createElement("div");
     function init() {
         for (let i = 0; i < Letters.length; i++) {
+            let div = document.createElement("div");
             div.innerText = Letters[i];
+            div.style.width = "40px";
+            div.style.height = "40px";
             div.style.textAlign = "center";
+            div.style.font = "bold 20px Courier,serif";
+            div.style.marginLeft = "5px";
+            div.style.marginTop = "5px";
+            div.style.padding = "6px";
+            div.style.color = "white";
+            div.style.backgroundColor = "#00334d";
+            div.style.fontSize = "2.3em";
+            div.style.display = "inline";
+            div.addEventListener("mousedown", pressKey);
+            document.body.appendChild(div);
         }
+    }
+    function inputBox() {
+        let inputBox = document.createElement("div");
     }
     function pressKey() {
     }
     function mouseClick() {
+    }
+    function drawLetter() {
     }
 })(a9 || (a9 = {}));
 //# sourceMappingURL=a9.js.map
