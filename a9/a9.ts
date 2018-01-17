adb89bdf1c9400e5be05eaeab2d99f81f8c4b9db
user <user@Natalie-PC.ghb.hs-furtwangen.de>
2018-01-17 21:05:23 +0100

    x

--- a9/a9.ts
+++ a9/a9.ts
@@ -6,19 +6,19 @@
     Code selbst geschrieben habe. Er wurde
     nicht kopiert und auch nicht diktiert.*/
 
-namespace a9   {
-    
+namespace a9 {
+
     window.addEventListener("load", init);
-    
-    
-    let Letters : string [] = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+
+    let Letters: string[] = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
     let output: string = "";
- 
-    
-    function init() :void {
+
+
+    function init(): void {
         for (let i = 0; i < Letters.length; i++) {
-       //Buchstaben
-            let div = document.createElement("div"); 
+            //Buchstaben
+            let div = document.createElement("div");
             div.innerText = Letters[i];
             div.style.width = "40px";
             div.style.height = "40px";
@@ -29,17 +29,17 @@ namespace a9   {
             div.style.padding = "6px";
             div.style.color = "white";
             div.style.backgroundColor = "#00334d";
-            div.style.display = "inline";  
+            div.style.display = "inline";
             div.id = Letters[i].toUpperCase(); //Werden groß geschrieben
             div.addEventListener("mousedown", mouseClick);
             document.body.appendChild(div);
-            }
-    inputBox();
         }
-    
-function inputBox() : void {
-    
-    let inputBox: HTMLDivElement = document.createElement("div");  
+        inputBox();
+    }
+
+    function inputBox(): void {
+
+        let inputBox: HTMLDivElement = document.createElement("div");
         inputBox.style.width = "80%";
         inputBox.style.margin = "5px";
         inputBox.style.height = "600px";
@@ -47,45 +47,45 @@ function inputBox() : void {
         inputBox.style.border = "solid #00334d 3px";
         inputBox.addEventListener("mousedown", drawLetter);
         document.body.appendChild(inputBox);
-    
+
     }
-    
-    
-function mouseClick(_event: MouseEvent) : void {
-   
+
+
+    function mouseClick(_event: MouseEvent): void {
+
         let div2: HTMLDivElement = <HTMLDivElement>_event.target;
         div2.style.color = "#00334d";
         div2.style.backgroundColor = "#99dfff";
         output = div2.id;
-        
+
     }
-    
-    
-function drawLetter(_event: MouseEvent) : void {
-    if (output == "")
-    return;
-    
-    let newDiv: HTMLDivElement = document.createElement("div");
-    newDiv.innerText = output;
-    newDiv.style.position = "absolute"; //sonst: Buchstaben erscheinen nach box(nach nächstem Element, standart (static))
-    newDiv.style.color = "#00334d";
-    newDiv.style.font = "bold 2.3em Courier,serif";
-    newDiv.style.left = _event.pageX + "px";
-    newDiv.style.top = _event.pageY + "px";
-    newDiv.addEventListener("mousedown", deleteDiv)
-    document.body.appendChild(newDiv);
 
-    let reset: HTMLDivElement = <HTMLDivElement>document.getElementById(output);
-    reset.style.color = "white";
-    reset.style.backgroundColor = "#00334d";
-   
 
-    }    
-    
-    function deleteDiv(_event: MouseEvent) : void{
+    function drawLetter(_event: MouseEvent): void {
+        if (output == "")
+            return;
+
+        let newDiv: HTMLDivElement = document.createElement("div");
+        newDiv.innerText = output;
+        newDiv.style.position = "absolute"; //sonst: Buchstaben erscheinen nach box(nach nächstem Element, standart (static))
+        newDiv.style.color = "#00334d";
+        newDiv.style.font = "bold 2.3em Courier,serif";
+        newDiv.style.left = _event.pageX + "px";
+        newDiv.style.top = _event.pageY + "px";
+        newDiv.addEventListener("mousedown", deleteDiv)
+        document.body.appendChild(newDiv);
+
+        let reset: HTMLDivElement = <HTMLDivElement>document.getElementById(output);
+        reset.style.color = "white";
+        reset.style.backgroundColor = "#00334d";
+
+
+    }
+
+    function deleteDiv(_event: MouseEvent): void {
         if (_event.altKey == true) {
             let letter: HTMLDivElement = <HTMLDivElement>_event.target;
             document.body.removeChild(letter);
         }
-        }   
- }
+    }
+}
