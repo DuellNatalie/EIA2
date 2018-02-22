12ee685ebe7961cffae8b9733e20d83acc64d086
user <user@Natalie-PC.Speedport_W_724V_09011603_05_010>
2018-02-22 05:44:57 +0100

    Wolken Animation

--- Endaufgabe/cloud.js
+++ Endaufgabe/cloud.js
@@ -6,18 +6,19 @@ var A;
         }
         draw() {
             A.crc2.beginPath();
-            A.crc2.arc(this.x, this.y, 23, 0, 2 * Math.PI);
-            A.crc2.fillStyle = "#ffffff";
+            A.crc2.arc(this.x, this.y, 28, 0, 2 * Math.PI);
+            A.crc2.globalAlpha = 0.95;
+            A.crc2.fillStyle = "#a6a6a6";
             A.crc2.fill();
             A.crc2.beginPath();
-            A.crc2.arc(this.x + 23, this.y - 13, 25, 0, 2 * Math.PI);
+            A.crc2.arc(this.x + 23, this.y - 13, 28, 0, 2 * Math.PI);
             A.crc2.fill();
             A.crc2.beginPath();
-            A.crc2.arc(this.x + 36, this.y + 4, 22, 0, 2 * Math.PI);
+            A.crc2.arc(this.x + 36, this.y + 4, 28, 0, 2 * Math.PI);
             A.crc2.fill();
         }
         move() {
-            if (this.x > 820) {
+            if (this.x > 1260) {
                 this.x = -20;
             }
             this.x += 1;
